0a00378b20d047c42838431dcc22f826408e45d7
susan <odiisusan@gmail.com>
2022-10-02 16:04:32 +0100

    hambuger menu

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 
-const hamburger = document.querySelector(".hamburger");
-const navManu = document.querySelector(".nav-menu");
-const subTopicAboutMe= document.querySelector(".subTopic-about-me");
-hamburger.addEventListener("click", () =>{
-    hamburger.classList.toggle("active");
-    navManu.classList.toggle("active");
-    subTopicAboutMe.classList.toggle("active")
-})
-document.querySelectorAll(".nav-link").forEach(n => n.
-    addEventListener("click",() =>{
-        hamburger.classList.remove("active");
-        navManu.classList.remove("active");
-    }))
+const menu = document.querySelector('.nav-menu')
+const hambuger = document.querySelector('.hambuger')
+const menuIcon = document.querySelector('.fa-bars')
+const closeIcon = document.querySelector('.fa-window-close')
+// const navLink = document.querySelector('.nav-link')
+const eachNavLink = document.querySelectorAll('.nav-link')
+const myLink = document.getElementById('myLink')
+
+const toggleMenu  = function(){
+    if(menu.classList.contains("showMenu")){
+        menu.classList.remove("showMenu");
+        menuIcon.style.display = "block";
+        closeIcon.style.display ="none"
+        
+    }else {
+        menu.classList.add("showMenu");
+        menuIcon.style.display = "none";
+        closeIcon.style.display ="block"
+        document.querySelector(".profile-image").style.marginTop = "25rem";
+        document.querySelector(".place-of-work-section").style.marginTop = "15rem";
+    }
+}
+
+
+hambuger.addEventListener('click', toggleMenu)
+
+// Functionality for individual nav-link
+eachNavLink.forEach(
+    function(navLink){
+        console.log(navLink)
+      navLink.addEventListener('click', toggleMenu)
+    //   console.log(eachNavLINK)  
+    console.log(navLink)
+    console.log(toggleMenu);
+    }
+    
+)
